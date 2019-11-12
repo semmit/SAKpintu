9947638fecaeaf96c01742d77e752f552f7e5d15
semmit <56271710+semmit@users.noreply.github.com>
2019-11-13 02:34:40 +0700

    Update marketplace-register.component.ts

--- src/app/marketplace-register/marketplace-register.component.ts
+++ src/app/marketplace-register/marketplace-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { request, getFile, getImage, getJSON, getString, HttpResponse } from "tns-core-modules/http";
 
 @Component({
   selector: 'ns-marketplace-register',
@@ -12,4 +13,36 @@ export class MarketplaceRegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  myFunction() {
+    console.log("myFunction");
+    request({
+      url: "https://sp.haloteman.com/API_MAIN/userRegister.php",
+      method: "POST",
+      headers: {
+        "HTTPMethod": "POST",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic"
+      },
+      content: JSON.stringify({
+        "Username": ("#username"),
+        "Password": ("#password"),
+        "email": ("email"),
+        "handphone": ("handphone"),
+        "no_ktp": ("no_ktp")
+      })
+    }).then((response: HttpResponse) => {
+      // Content property of the response is HttpContent
+      // The toString method allows you to get the response body as string.
+      const str = response.content.toString();
+      console.log(str);
+      // The toJSON method allows you to parse the received content to JSON object
+      // var obj = response.content.toJSON();
+      // The toImage method allows you to get the response body as ImageSource.
+      // var img = response.content.toImage();
+    }, (e) => {
+    });
+
+    // this.userService.logout();
+    // this.routerExtensions.navigate(["/login"], { clearHistory: true });
+  }
 }
