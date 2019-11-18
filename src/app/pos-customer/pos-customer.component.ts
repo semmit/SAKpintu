43bd31611658ec9ccbe830ffd9f82aea1186bc39
semmit <56271710+semmit@users.noreply.github.com>
2019-11-19 03:45:07 +0700

    Add files via upload
    POS-CUSTOMER

---
+++ src/app/pos-customer/pos-customer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+    selector: "pos-customer",
+    templateUrl: "./pos-customer.component.html",
+  	styleUrls: ['./pos-dataitem.component.css'],
+  	moduleId: module.id,
+})
+export class PosCustomerComponent implements OnInit {
+    items: Array<IDataItem>;
+
+    constructor(private _itemService: DataService) { }
+
+    ngOnInit(): void {
+        this.items = this._itemService.getItems();
+    }
+}
