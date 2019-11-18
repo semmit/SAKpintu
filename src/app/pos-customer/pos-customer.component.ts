aa84f4b8ffbacd35b12d00b623e02280c37eb12d
Hans Tedjakusuma <hansted@users.noreply.github.com>
2019-11-19 06:47:33 +0700

    sudah ditambah import

--- src/app/pos-customer/pos-customer.component.ts
+++ src/app/pos-customer/pos-customer.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { DataService, IDataItem } from "../shared/data.service";
 
 @Component({
     selector: "pos-customer",
     templateUrl: "./pos-customer.component.html",
-  	styleUrls: ['./pos-dataitem.component.css'],
-  	moduleId: module.id,
+    styleUrls: ['./pos-dataitem.component.css'],
+    moduleId: module.id,
 })
 export class PosCustomerComponent implements OnInit {
     items: Array<IDataItem>;
