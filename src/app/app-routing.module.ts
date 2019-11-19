253ffdbf5ef557b8cf49b22bfb95239b6cf614ce
semmit <56271710+semmit@users.noreply.github.com>
2019-11-19 12:16:09 +0700

    Update app-routing.module.ts
    penambahan pos-cutomer dan pengurutan nama file component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { Routes } from "@angular/router";
 
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
-
 import { PosDashboardComponent } from "./pos-dashboard/pos-dashboard.component";
+import { PosCustomerComponent } from './pos-customer/pos-customer.component';
+
 import { UserLoginComponent } from "./user-login/user-login.component";
 import { LoginPosComponent } from "~/app/login-pos/login-pos.component";
 import { LoginMarketplaceComponent } from "~/app/login-marketplace/login-marketplace.component";
+
 import { MarketplaceComponent } from "~/app/marketplace/marketplace.component";
 import { MarketplaceRegisterComponent } from "./marketplace-register/marketplace-register.component";
 import { MarketplaceProductInfoComponent } from "./marketplace-product-info/marketplace-product-info.component";
 import { MarketplaceShortingComponent } from "./marketplace-shorting/marketplace-shorting.component";
+
 import { MarketplaceUserComponent } from "./marketplace-user/marketplace-user.component";
 import { MarketplaceUserDataAkunComponent } from "./marketplace-user-datatakun/marketplace-user-datatakun.component";
 import { MarketplaceUserDataTokoComponent } from "./marketplace-user-datatoko/marketplace-user-datatoko.component";
@@ -24,13 +27,14 @@ const routes: Routes = [
     { path: "items", component: ItemsComponent },
     { path: "item/:id", component: ItemDetailComponent },
     { path: "pos-dashboard", component: PosDashboardComponent },
+    { path: "pos-dataitem", redirectTo: "/items", pathMatch: "full" },
+    { path: "pos-customer", component: PosCustomerComponent },
     { path: "login", component: UserLoginComponent },
     { path: "login-pos", component: LoginPosComponent },
     { path: "login-marketplace", component: LoginMarketplaceComponent },
     { path: "register-marketplace", component: MarketplaceRegisterComponent },
     { path: "marketplace", component: MarketplaceComponent },
     { path: "marketplace-product-info", component: MarketplaceProductInfoComponent },
-    { path: "pos-dataitem", redirectTo: "/items", pathMatch: "full" },
     { path: "marketplace-shorting", component: MarketplaceShortingComponent },
     { path: "marketplace-user", component: MarketplaceUserComponent },
     { path: "marketplace-user-datatakun", component: MarketplaceUserDataAkunComponent },
