b2dd02b1ac6d4769318a6403510ee00b613844c7
Hans Tedjakusuma <hansted@users.noreply.github.com>
2019-11-20 01:42:46 +0700

    penamaan

--- src/app/pos_csmsitem/pos_csmsitem_daftar.component.ts
+++ src/app/pos_csmsitem/pos_csmsitem_daftar.component.ts
@@ -3,16 +3,16 @@ import { csmsitem_datalist } from "./csmsitem_datalist";
 import { csmsitem_dataset } from "./csmsitem_dataset";
 
 @Component({
-    selector: "ns_pos_csmsitem_daftar",
+    selector: "ns-pos_csmsitem_daftar",
     templateUrl: "./pos_csmsitem_daftar.component.html",
     styleUrls: ['./../_css/base.component.css']
 })
 export class pos_csmsitem_daftar implements OnInit {
-    // datalist: Array<csmsitem_dataset>;
+    datalist: Array<csmsitem_dataset>;
 
     constructor(private main_datalist: csmsitem_datalist) { }
 
     ngOnInit(): void {
-        // this.datalist = this.main_datalist.getdatalist();
+        this.datalist = this.main_datalist.getdatalist();
     }
 }
