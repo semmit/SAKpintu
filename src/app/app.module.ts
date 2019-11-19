50049849b85278236e2875fd23972dba4db85db6
semmit <56271710+semmit@users.noreply.github.com>
2019-11-19 12:15:15 +0700

    Update app.module.ts
    Nama sudah diurutkan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FocusDirective } from "./shared/FocusDirective";
+
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
 import { PosDashboardComponent } from './pos-dashboard/pos-dashboard.component';
-import { UserLoginComponent } from './user-login/user-login.component';
+import { PosDataitemComponent } from './pos-dataitem/pos-dataitem.component';
+import { PosCustomerComponent } from './pos-customer/pos-customer.component';
 
 import { NativeScriptMaterialCardViewModule } from 'nativescript-material-cardview/angular';
 import { LoginPosComponent } from './login-pos/login-pos.component';
 import { LoginMarketplaceComponent } from './login-marketplace/login-marketplace.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
-import { PosDataitemComponent } from './pos-dataitem/pos-dataitem.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -22,6 +24,7 @@ import { MarketplaceRegisterComponent } from './marketplace-register/marketplace
 import { MarketplaceRegisterConfirmComponent } from './marketplace-register-confirm/marketplace-register-confirm.component';
 import { MarketplaceProductInfoComponent } from './marketplace-product-info/marketplace-product-info.component';
 import { MarketplaceShortingComponent } from "./marketplace-shorting/marketplace-shorting.component";
+
 import { MarketplaceUserComponent } from "./marketplace-user/marketplace-user.component";
 import { MarketplaceUserDataAkunComponent } from "./marketplace-user-datatakun/marketplace-user-datatakun.component";
 import { MarketplaceUserDataTokoComponent } from "./marketplace-user-datatoko/marketplace-user-datatoko.component";
@@ -55,11 +58,12 @@ import { MarketplaceUserDaftarOrderComponent } from "./marketplace-user-daftaror
         ItemsComponent,
         ItemDetailComponent,
         PosDashboardComponent,
+        PosDataitemComponent,
+        PosCustomerComponent,
         UserLoginComponent,
         LoginPosComponent,
         LoginMarketplaceComponent,
         MarketplaceComponent,
-        PosDataitemComponent,
         MarketplaceRegisterComponent,
         MarketplaceRegisterConfirmComponent,
         MarketplaceProductInfoComponent,
