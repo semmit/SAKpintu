43d11915c12d44b2233b886e72c5e5d12eacfb67
semmit <56271710+semmit@users.noreply.github.com>
2019-11-19 17:50:21 +0700

    Add files via upload

---
+++ src/app/shared/datacustomer.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from "@angular/core";
+
+export interface IDataCustomer {
+    id: number;
+    namatoko: string;
+    namalengkap: string;
+    nohandphone: string;
+}
+
+@Injectable({
+    providedIn: "root"
+})
+export class DataCustomer {
+
+    private items = new Array<IDataCustomer>(
+        {
+            id: 1,
+            namatoko: "Nama Toko",
+            namalengkap: "Nama Lengkap",
+            nohandphone: "0980980980980980"
+        },
+        {
+            id: 2,
+            namatoko: "Nama Toko2",
+            namalengkap: "Nama Lengkap2",
+            nohandphone: "0980980980980980"
+        },
+        {
+            id: 3,
+            namatoko: "Nama Toko3",
+            namalengkap: "Nama Lengkap3",
+            nohandphone: "0980980980980980"
+        },
+        {
+            id: 4,
+            namatoko: "Nama Toko4",
+            namalengkap: "Nama Lengkap4",
+            nohandphone: "0980980980980980"
+        },
+        {
+            id: 5,
+            namatoko: "Nama Toko5",
+            namalengkap: "Nama Lengkap5",
+            nohandphone: "0980980980980980"
+        },
+        {
+            id: 6,
+            namatoko: "Nama Toko6",
+            namalengkap: "Nama Lengkap6",
+            nohandphone: "0980980980980980"
+        },
+        {
+            id: 7,
+            namatoko: "Nama Toko7",
+            namalengkap: "Nama Lengkap7",
+            nohandphone: "0980980980980980"
+        },
+        {
+            id: 8,
+            namatoko: "Nama Toko8",
+            namalengkap: "Nama Lengkap8",
+            nohandphone: "0980980980980980"
+        },
+    );
+
+    getItems(): Array<IDataCustomer> {
+        return this.items;
+    }
+
+    getItem(id: number): IDataCustomer {
+        return this.items.filter((item) => item.id === id)[0];
+    }
+}
