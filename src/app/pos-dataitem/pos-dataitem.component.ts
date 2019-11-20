a6cdf56d42aa6b1d37c9acc1e5f342a4dfead1f8
semmit <56271710+semmit@users.noreply.github.com>
2019-11-21 01:27:36 +0700

    Add files via upload
    POSdataitem dari dashboard button ke 3

---
+++ src/app/pos-dataitem/pos-dataitem.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Page, getViewById } from 'tns-core-modules/ui/page';
+import { RouterExtensions } from 'nativescript-angular/router';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { ItemService, Item } from "../shared/item.service";
+
+@Component({
+    selector: "ns-pos-dataitem",
+    templateUrl: './pos-dataitem.component.html',
+    styleUrls: ['./../_css/base.component.css'],
+    moduleId: module.id,
+})
+export class PosDataItemComponent implements OnInit {
+    items: Array<Item>;
+
+    constructor(private _itemService: ItemService, private page: Page) { 
+    this.page.actionBarHidden = true;
+    }
+
+    ngOnInit(): void {
+        this.items = this._itemService.getItems();
+    }
+}
