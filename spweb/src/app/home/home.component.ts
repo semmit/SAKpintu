c6affe9eb25b36061711a91040435f84c763467b
semmit <56271710+semmit@users.noreply.github.com>
2019-11-29 01:14:37 +0700

    Add files via upload

---
+++ spweb/src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from "@angular/core";
+import { Page, getViewById } from 'tns-core-modules/ui/page';
+import { WebView, LoadEventData } from "tns-core-modules/ui/web-view";
+
+@Component({
+    selector: "Home",
+    templateUrl: "./home.component.html"
+})
+export class HomeComponent implements OnInit {
+webViewSrc = "https://sp.haloteman.com/";
+	
+	constructor(private page: Page) { 
+    this.page.actionBarHidden = true;
+    }
+
+    onLoadStarted(args: LoadEventData) {
+        const webView = args.object as WebView;
+
+        if (!args.error) {
+            console.log("Load Start");
+            console.log(`EventName: ${args.eventName}`);
+            console.log(`NavigationType: ${args.navigationType}`);
+            console.log(`Url: ${args.url}`);
+        } else {
+            console.log(`EventName: ${args.eventName}`);
+            console.log(`Error: ${args.error}`);
+        }
+    }
+
+    onLoadFinished(args: LoadEventData) {
+        const webView = args.object as WebView;
+
+        if (!args.error) {
+            console.log("Load Finished");
+            console.log(`EventName: ${args.eventName}`);
+            console.log(`NavigationType: ${args.navigationType}`);
+            console.log(`Url: ${args.url}`);
+        } else {
+            console.log(`EventName: ${args.eventName}`);
+            console.log(`Error: ${args.error}`);
+        }
+    }
+}
