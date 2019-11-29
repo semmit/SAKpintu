b84eba2602492ce37c19665b5a81fb25c5c95948
semmit <56271710+semmit@users.noreply.github.com>
2019-11-30 05:10:29 +0700

    Update pos-dashboard.component.ts
    add side drawer

--- src/app/pos-dashboard/pos-dashboard.component.ts
+++ src/app/pos-dashboard/pos-dashboard.component.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit, ChangeDetectorRef } from "@angular/core";
+import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular";
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 import { Page } from 'tns-core-modules/ui/page';
 
 @Component({
+  moduleId: module.id,
   selector: 'ns-pos-dashboard',
   templateUrl: './pos-dashboard.component.html',
   styleUrls: ['./pos-dashboard.component.css']
 })
-export class PosDashboardComponent implements OnInit {
+export class PosDashboardComponent implements AfterViewInit, OnInit {
+	private _mainContentText: string;
+	
+	  constructor(private _changeDetectionRef: ChangeDetectorRef) {
+    }
 
-  constructor(private page: Page) { 
-    // this.page.actionBarHidden = true;
-  }
+    @ViewChild(RadSideDrawerComponent, { static: false }) public drawerComponent: RadSideDrawerComponent;
+    private drawer: RadSideDrawer;
 
-  ngOnInit() {
-  }
+    ngAfterViewInit() {
+        this.drawer = this.drawerComponent.sideDrawer;
+        this._changeDetectionRef.detectChanges();
+    }
 
+    ngOnInit() {
+        this.mainContentText = "SideDrawer for NativeScript can be easily setup in the HTML definition of your page by defining tkDrawerContent and tkMainContent. The component has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.";
+    }
+
+    get mainContentText() {
+        return this._mainContentText;
+    }
+
+    set mainContentText(value: string) {
+        this._mainContentText = value;
+    }
+
+    public openDrawer() {
+        this.drawer.showDrawer();
+    }
+
+    public onCloseDrawerTap() {
+        this.drawer.closeDrawer();
+    }
 }
