0de3f5655cd833bc6b8f9aba70c96b0c02ed9296
Hans Tedjakusuma <57654613+hansted@users.noreply.github.com>
2019-12-01 01:26:03 +0700

    Update app.component.ts

--- spweb/src/app/app.component.ts
+++ spweb/src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnChanges, OnDestroy } from "@angular/core";
 import * as application from "tns-core-modules/application";
-import * as frame from "tns-core-modules/ui/frame"
+import { Frame } from "tns-core-modules/ui/frame/frame";
 
 @Component({
     selector: "ns-app",
     templateUrl: "./app.component.html"
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnChanges, OnDestroy {
 
     public constructor() { }
 
+    public ngOnChanges() {
+        console.log("OnChanges");
+    }
+
+    public ngOnDestroy() {
+        console.log("OnDestroy"); 
+        application.android.off(application.AndroidApplication.activityBackPressedEvent);
+    }
+
     public ngOnInit() {
+        application.android.off(application.AndroidApplication.activityBackPressedEvent);
         application.android.on(application.AndroidApplication.activityBackPressedEvent, (args: any) => {
-            const page = frame.topmost().currentPage;
+            const page = Frame.topmost().currentPage;
             if (page.hasListeners(application.AndroidApplication.activityBackPressedEvent)) {
                 args.cancel = true;
                 page.notify({
@@ -20,7 +30,6 @@ export class AppComponent implements OnInit {
                     object: page
                 });
             }
-
         });
     }
 
