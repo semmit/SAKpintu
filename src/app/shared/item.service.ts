50c1df41f8d8eb83ca6925b6cd72a547c0866880
Dionisius Agung Putrantio <56271710+semmit@users.noreply.github.com>
2019-12-05 06:02:03 +0700

    Update item.service.ts

--- src/app/shared/item.service.ts
+++ src/app/shared/item.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 
 export interface Item {
     id: number;
+    src: string;
     name: string;
     desc: string;
     satuan: string;
@@ -14,21 +15,21 @@ export interface Item {
 })
 export class ItemService {
     private items = new Array<Item>(
-        { id: 1, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 2, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 3, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 4, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 5, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 6, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 7, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 8, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 9, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 10, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 11, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 12, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 13, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 14, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
-        { id: 15, name: "Nama Item", desc: "aaaaabbbbb yeesss", price: "999999", satuan:"pcs", stock: "999999" },
+        { id: 1, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai%20bola%20hijau.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 2, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai03.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 3, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai bola emas.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 4, src: "https://sp.haloteman.com/_img/sampleprod/lowres/palata.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 5, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai04.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 6, src: "https://sp.haloteman.com/_img/sampleprod/lowres/ciok o.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 7, src: "https://sp.haloteman.com/_img/sampleprod/lowres/ragi tempe.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 8, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai%20bola%20hijau.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 9, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai03.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 10, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai bola emas.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 11, src: "https://sp.haloteman.com/_img/sampleprod/lowres/palata.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 12, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai04.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 13, src: "https://sp.haloteman.com/_img/sampleprod/lowres/ciok o.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 14, src: "https://sp.haloteman.com/_img/sampleprod/lowres/ragi tempe.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
+        { id: 15, src: "https://sp.haloteman.com/_img/sampleprod/lowres/kedelai%20bola%20hijau.jpg", name: "Nama csmsitem_dataset", desc: "aaaaabbbbb yeesss", price: "999999", satuan: "pcs", stock: "999999" },
     );
 
     getItems(): Array<Item> {
